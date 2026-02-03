da1630c641443a48b24d4a09ac77fffe6a2539af
Mantis <mjbgtaad56@gmail.com>
2026-02-03 16:11:11 -0500

    Theoretically finished relay

--- src/LobbyDO.ts
+++ src/LobbyDO.ts
@@ -1,21 +1,246 @@
 import { DurableObject } from "cloudflare:workers";
+import { RelayMessage } from "./RelayMessage";
 
 export class LobbyDO extends DurableObject<Env> {
+    private code: string;
+    private server: WebSocket | null = null;
+    private peers: Map<number, WebSocket> = new Map<number, WebSocket>();
+	private nextPeer: number = 0;
+	private currentPeers: number = 0;
+
     constructor(ctx: DurableObjectState<{}>, env: Env) {
         super(ctx, env);
+        this.code = ctx.id.name as string;
     }
 
-    async connectServer(request: Request): Promise<null> {
+    async connectServer(request: Request): Promise<Response> {
+        // Require that the request is a WebSocket upgrade
+        const upgradeHeader = request.headers.get('Upgrade');
+        if (!upgradeHeader || upgradeHeader !== "websocket") {
+            return new Response("Expected WebSocket", { status: 426 });
+        }
+
+        if (this.server !== null) {
+            return new Response("A server is already connected to this lobby", { status: 403 });
+        }
+
+        // Create a WebSocket pair
         const { 0: client, 1: server } = new WebSocketPair();
-        return null;
+        server.accept();
+        this.server = server;
+        console.log(`Server connected to lobby "${this.code}"`);
+        
+        server.addEventListener("close", this.onServerClose.bind(this));
+        server.addEventListener("message", this.onServerMessage.bind(this));
+        server.addEventListener("error", this.onServerError.bind(this));
+
+        this.server.send(RelayMessage.serialize({
+            type: RelayMessage.Type.CODE,
+            direction: RelayMessage.Direction.RELAY_TO_SERVER,
+            code: this.code
+        }));
+
+        return new Response(null, { status: 101, webSocket: client });
     }
 
-    async connectClient(request: Request): Promise<null> {
+    async connectClient(request: Request): Promise<Response> {
+        if (this.server === null) {
+            return new Response("No server connected to this lobby", { status: 404 });
+        }
+
+        // Require that the request is a WebSocket upgrade
+        const upgradeHeader = request.headers.get('Upgrade');
+        if (!upgradeHeader || upgradeHeader !== "websocket") {
+            return new Response("Expected WebSocket", { status: 426 });
+        }
+
+		const id = this.getNextAvailablePeer();
+
+		if (id == -1) {
+			return new Response("No available peer slots", { status: 403 });
+		}
+        
+        // Create a WebSocket pair
         const { 0: client, 1: server } = new WebSocketPair();
-        return null;
+		this.peers.set(id, server);
+		this.currentPeers |= 1 << id;
+        server.accept();
+        console.log(`Client ${id} connected to lobby "${this.code}"`);
+
+		server.addEventListener("close", this.onClientClose(id).bind(this));
+		server.addEventListener("message", this.onClientMessage(id).bind(this));
+		server.addEventListener("error", this.onClientError(id).bind(this));
+
+		this.server.send(RelayMessage.serialize({
+			direction: RelayMessage.Direction.RELAY_TO_SERVER,
+			type: RelayMessage.Type.CONNECT,
+			id: id
+		}));
+
+        return new Response(null, { status: 101, webSocket: client });
     }
+    
+	private getNextAvailablePeer(): number {
+		for (let i = 0; i < 32; i++) {
+			let id = (this.nextPeer + i) % 32;
+			if ((this.currentPeers >>> id & 0x1) === 0) {
+				this.nextPeer = (id + 1) % 32;
+				return id;
+			}
+		}
+
+		return -1;
+	}
 
     async hasConnectedServer(): Promise<boolean> {
-        return false;
+        return this.server !== null;
     }
+
+    private onServerClose(event: CloseEvent) {
+        console.log(`Server disconnected from lobby ${this.code}`);
+        this.server = null;
+        this.currentPeers = 0;
+
+        // Disconnect all connected peers
+        this.peers.forEach((peer) => {
+            peer.close();
+        });
+        this.peers.clear();
+    }
+
+	private onServerMessage(event: MessageEvent): void {
+        const data = event.data;
+		if (data == undefined)
+			return;
+
+		var message = RelayMessage.deserialize(data as Uint8Array, RelayMessage.Direction.SERVER_TO_RELAY);
+
+		switch (message.type) {
+			case RelayMessage.Type.DATA:
+				this.forwardData(message as RelayMessage.ServerDataIn);
+				break;
+
+			case RelayMessage.Type.DISCONNECT:
+				this.kickClient(message.id);
+				break;
+
+			case RelayMessage.Type.CONNECT:
+				const msg: RelayMessage.InformConnectClient = {
+					direction: RelayMessage.Direction.RELAY_TO_CLIENT,
+					type: RelayMessage.Type.CONNECT
+				}
+				const acceptMsg = message as RelayMessage.AcceptConnection;
+				const peer = this.peers.get(acceptMsg.id);
+					if (peer != undefined)
+						peer.send(RelayMessage.serialize(msg));
+					else
+						console.warn(`Relay "${this.code}": Cannot send accept to invalid peer ID "${acceptMsg.id}"`);
+				break;
+
+			default:
+				console.warn(`Relay "${this.code}": Unexpected message type from server: ${(data as Uint8Array)[0]}`)
+		}
+	}
+
+	private kickClient(ID: number): void {
+		let ws = this.peers.get(ID);
+
+		if (ws == undefined) {
+			console.warn(`Relay "${this.code}": Cannot kick client "${ID}" They do not exist.`);
+			return;
+		}
+
+		ws.close();
+		this.peers.delete(ID);
+		this.currentPeers &= ~(1 << ID);
+
+		let msg: RelayMessage.InformDisconnect = {
+			direction: RelayMessage.Direction.RELAY_TO_SERVER,
+			type: RelayMessage.Type.DISCONNECT,
+			id: ID
+		};
+
+		this.server!.send(RelayMessage.serialize(msg))
+	}
+
+	private onServerError(error: ErrorEvent): void {
+		console.warn(`Relay "${this.code}": Server web socket error: ${error.message}`);
+	}
+    
+	private onClientClose(ID: number): (event: CloseEvent) => void {
+		return (event: CloseEvent) => {
+			console.log(`Relay "${this.code}": Client "${ID}" disconnected`);
+
+			let msg: RelayMessage.InformDisconnect = {
+				direction: RelayMessage.Direction.RELAY_TO_SERVER,
+				type: RelayMessage.Type.DISCONNECT,
+				id: ID
+			};
+
+			this.server!.send(RelayMessage.serialize(msg));
+
+			this.peers.delete(ID);
+			this.currentPeers &= ~(1 << ID);
+		};
+	}
+
+	private onClientMessage(ID: number): (event: MessageEvent) => void {
+		return (event: MessageEvent) => {
+            const data = event.data;
+			if (data == undefined)
+				return;
+
+			var message = RelayMessage.deserialize(data as Uint8Array, RelayMessage.Direction.CLIENT_TO_RELAY);
+
+			switch (message.type) {
+				case RelayMessage.Type.DATA:
+					this.forwardData(message as RelayMessage.ClientData, ID);
+					return;
+				default:
+					console.warn(`Relay "${this.code}": Unexpected message type from client: ${(data as Uint8Array)[0]}`);
+					return;
+			}
+		};
+	}
+
+	private onClientError(ID: number): (error: ErrorEvent) => void {
+		return (error: ErrorEvent) => {
+			console.warn(`Relay "${this.code}": Client "${ID}" web socket error: ${error.message}`);
+		}
+	}
+
+	private forwardData(message: RelayMessage.ServerDataIn): void;
+	private forwardData(message: RelayMessage.ClientData, source: number): void;
+	private forwardData(message: RelayMessage.ClientData | RelayMessage.ServerDataIn, source?: number): void {
+		if (message.direction === RelayMessage.Direction.SERVER_TO_RELAY) {
+			const newMessage: RelayMessage.ClientData = {
+				type: message.type,
+				direction: RelayMessage.Direction.RELAY_TO_CLIENT,
+				data: message.data
+			}
+
+			const serialMessage = RelayMessage.serialize(newMessage);
+
+			for (let i = 0; i < 32; i++) {
+				const shouldSend: boolean = (this.currentPeers >>> i & 1) === 1;
+
+				if (shouldSend) {
+					const peer = this.peers.get(i);
+					if (peer != undefined)
+						peer.send(serialMessage);
+					else
+						console.warn(`Relay "${this.code}": Cannot send data to invalid peer ID "${i}"`);
+				}
+			}
+		} else if (message.direction === RelayMessage.Direction.CLIENT_TO_RELAY) {
+			const newMessage: RelayMessage.ServerDataOut = {
+				type: message.type,
+				direction: RelayMessage.Direction.RELAY_TO_SERVER,
+				source: source as number,
+				data: message.data
+			}
+
+			this.server!.send(RelayMessage.serialize(newMessage));
+		}
+	}
 }
---
+++ src/RelayMessage.ts
@@ -0,0 +1,277 @@
+
+export namespace RelayMessage {
+    export enum Type {
+        UNDEFINED = -1,
+        DATA,
+        CONNECT,
+        DISCONNECT,
+        CODE,
+        PING,
+        
+        MAXIMUM
+    }
+
+    export enum Direction {
+        SERVER_TO_RELAY,
+        RELAY_TO_SERVER,
+        CLIENT_TO_RELAY,
+        RELAY_TO_CLIENT
+    }
+
+    export type RELAY_TO_ANY =
+        Direction.RELAY_TO_CLIENT |
+        Direction.RELAY_TO_SERVER;
+
+    export type ANY_TO_RELAY =
+        Direction.CLIENT_TO_RELAY |
+        Direction.SERVER_TO_RELAY;
+
+    export type SERVER_DIRECTION =
+        Direction.SERVER_TO_RELAY |
+        Direction.RELAY_TO_SERVER;
+
+    export type CLIENT_DIRECTION =
+        Direction.CLIENT_TO_RELAY |
+        Direction.RELAY_TO_CLIENT;
+
+    export type Undefined = {
+        type: Type.UNDEFINED,
+        direction: Direction
+    }
+
+    export type Ping = {
+        type: Type.PING,
+        direction: Direction
+    }
+
+    export type SendCode = {
+        type: Type.CODE,
+        direction: Direction.RELAY_TO_SERVER,
+        code: string
+    }
+
+    export type InformConnectServer = {
+        type: Type.CONNECT,
+        direction: Direction.RELAY_TO_SERVER,
+        id: number
+    }
+
+    export type AcceptConnection = {
+        type: Type.CONNECT,
+        direction: Direction.SERVER_TO_RELAY,
+        id: number
+    }
+
+    export type InformConnectClient = {
+        type: Type.CONNECT,
+        direction: Direction.RELAY_TO_CLIENT
+    }
+
+    export type ServerDataIn = {
+        type: Type.DATA,
+        direction: Direction.SERVER_TO_RELAY,
+        destinations: number,
+        data: Uint8Array
+    }
+
+    export type ServerDataOut = {
+        type: Type.DATA,
+        direction: Direction.RELAY_TO_SERVER,
+        source: number,
+        data: Uint8Array
+    }
+
+    export type ClientData = {
+        type: Type.DATA,
+        direction: CLIENT_DIRECTION,
+        data: Uint8Array
+    }
+
+    export type KickClient = {
+        type: Type.DISCONNECT,
+        direction: Direction.SERVER_TO_RELAY,
+        id: number
+    }
+
+    export type InformDisconnect = {
+        type: Type.DISCONNECT,
+        direction: RELAY_TO_ANY,
+        id: number
+    }
+
+    export function deserialize(data: Uint8Array, direction: Direction): RelayMessage {
+        var undef: Undefined = {
+            direction: direction,
+            type: Type.UNDEFINED
+        }
+
+        if (data.length == 0)
+            return undef;
+
+        var messageType: Type = Type.UNDEFINED;
+
+        if (data[0] as number < Type.MAXIMUM.valueOf())
+            messageType = data[0] as Type;
+        
+        switch(messageType) {
+            case Type.PING:
+                return {
+                    type: messageType,
+                    direction: direction
+                };
+
+            case Type.DATA:
+                switch (direction) {
+                    case Direction.RELAY_TO_CLIENT:
+                    case Direction.CLIENT_TO_RELAY:
+                        return {
+                            type: messageType,
+                            direction: direction,
+                            data: data.subarray(1)
+                        };
+                    case Direction.SERVER_TO_RELAY:
+                        return {
+                            type: messageType,
+                            direction: direction,
+                            destinations: parseS32(data.subarray(1,5)),
+                            data: data.subarray(5)
+                        }
+                    case Direction.RELAY_TO_SERVER:
+                        return {
+                            type: messageType,
+                            direction: direction,
+                            source: data[1] as number,
+                            data: data.subarray(2)
+                        }
+                }
+            case Type.CONNECT:
+                switch (direction) {
+                    case Direction.CLIENT_TO_RELAY:
+                        return undef;
+                    case Direction.RELAY_TO_CLIENT:
+                        return {
+                            type: messageType,
+                            direction: direction
+                        };
+                    case Direction.SERVER_TO_RELAY:
+                    case Direction.RELAY_TO_SERVER:
+                        return {
+                            type: messageType,
+                            direction: direction,
+                            id: data[1] as number
+                        };
+                }
+
+            case Type.DISCONNECT:
+                switch (direction) {
+                    case Direction.CLIENT_TO_RELAY:
+                    case Direction.RELAY_TO_CLIENT:
+                        return undef;
+                    case Direction.SERVER_TO_RELAY:
+                    case Direction.RELAY_TO_SERVER:
+                        return {
+                            type: messageType,
+                            direction: direction,
+                            id: data[1] as number
+                        };
+                }
+
+            case Type.CODE:
+                if (direction == Direction.RELAY_TO_SERVER)
+                    return {
+                        type: messageType,
+                        direction: direction,
+                        code: new TextDecoder().decode(data.subarray(1))
+                    };
+
+                return undef;
+        }
+
+        return {
+            direction: direction as ANY_TO_RELAY,
+            type: Type.UNDEFINED
+        };
+    }
+
+    export function serialize(data: RelayMessage): Uint8Array {
+        var result: number[] = [];
+
+        if (data.type == Type.UNDEFINED)
+            throw Error("Refusal to serialize undefined message");
+
+        result.push(data.type.valueOf());
+
+        switch (data.type) {
+            case Type.DATA:
+                switch (data.direction) {
+                    case Direction.CLIENT_TO_RELAY:
+                    case Direction.RELAY_TO_CLIENT:
+                        break;
+                    case Direction.RELAY_TO_SERVER:
+                        result.push(data.source);
+                        break;
+                    case Direction.SERVER_TO_RELAY:
+                        result.push(...encodeS32(data.destinations))
+                        break;
+                }
+                result.push(...data.data);
+                break;
+
+            case Type.CONNECT:
+                if (data.direction == Direction.RELAY_TO_SERVER || data.direction == Direction.SERVER_TO_RELAY)
+                    result.push(data.id);
+                break;
+
+            case Type.DISCONNECT:
+                result.push(data.id);
+                break;
+
+            case Type.CODE:
+                result.push(...new TextEncoder().encode(data.code))
+                break;
+        }
+
+        return new Uint8Array(result);
+    }
+
+    function parseS32(data: Uint8Array): number {
+        if (data.length < 4)
+            throw Error("Cannot parse int 32 from less than 4 bytes");
+
+        var n1 = data[0] as number;
+        var n2 = data[1] as number;
+        var n3 = data[2] as number;
+        var n4 = data[3] as number;
+
+        return (
+            (n1 << 0) |
+            (n2 << 8) |
+            (n3 << 16) |
+            (n4 << 24)
+        );
+    }
+
+    function encodeS32(data: number): Uint8Array {
+        var result: Uint8Array = new Uint8Array(4);
+
+        result[0] = data >> 0 & 0xff;
+        result[1] = data >> 8 & 0xff;
+        result[2] = data >> 16 & 0xff;
+        result[3] = data >> 24 & 0xff;
+
+        return result;
+    }
+}
+
+export type RelayMessage =
+    RelayMessage.Undefined |
+    RelayMessage.Ping |
+    RelayMessage.SendCode |
+    RelayMessage.InformConnectServer |
+    RelayMessage.InformConnectClient |
+    RelayMessage.AcceptConnection |
+    RelayMessage.ServerDataIn |
+    RelayMessage.ServerDataOut |
+    RelayMessage.ClientData |
+    RelayMessage.KickClient |
+    RelayMessage.InformDisconnect;
